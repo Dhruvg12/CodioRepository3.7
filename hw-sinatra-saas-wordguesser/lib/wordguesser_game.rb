e8ce87e6eec3812ac48b33335c9ad44c431b871a
Dhruv Gupta <dsg8657@nyu.edu>
2025-10-01 02:19:41 +0000

    assigment changes

--- hw-sinatra-saas-wordguesser/lib/wordguesser_game.rb
+++ hw-sinatra-saas-wordguesser/lib/wordguesser_game.rb
@@ -1,11 +1,14 @@
 class WordGuesserGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
+  attr_reader :word, :guesses, :wrong_guesses
 
   # Get a word from remote "random word" service
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -20,4 +23,32 @@ class WordGuesserGame
       return http.post(uri, "").body
     end
   end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || letter !~ /\A[a-zA-Z]\z/
+
+    letter = letter.downcase
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    if @word.downcase.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    true
+  end
+
+  # Reveal guessed letters, hide others with '-'
+  def word_with_guesses
+    @word.chars.map { |ch| @guesses.include?(ch.downcase) ? ch : '-' }.join
+  end
+
+  # Typical game-state helper the specs often use
+  # :win if all letters revealed, :lose if 7 wrong guesses, else :play
+  def check_win_or_lose
+    return :win  if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
+  end
+
 end
